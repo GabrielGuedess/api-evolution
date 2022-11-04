b45f11e8f46bfd89fdeb8023f795feb0c2b0ed07
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-11-04 20:04:02 -0300

    feat: add new routes /id/:id, /slug/:slug and /favorite in game

--- src/routes/game.routes.ts
+++ src/routes/game.routes.ts
@@ -5,6 +5,9 @@ import multer from 'multer';
 import { CreateFavoriteGameController } from 'modules/games/useCases/createFavoriteGame/CreateFavoriteGameController';
 import { CreateGameController } from 'modules/games/useCases/createGame/CreateGameController';
 import { DeleteFavoriteGameController } from 'modules/games/useCases/deleteFavoriteGame/DeleteFavoriteGameController';
+import { FindFavoritesGamesController } from 'modules/games/useCases/findFavoritesGames/FindFavoritesGamesController';
+import { FindGameByIdController } from 'modules/games/useCases/findGameById/FindGameByIdController';
+import { FindGameBySlugController } from 'modules/games/useCases/findGameBySlug/FindGameBySlugController';
 import { FindGamesController } from 'modules/games/useCases/findGames/FindGamesController';
 
 import uploadConfig from 'config/upload';
@@ -14,6 +17,9 @@ const upload = multer(uploadConfig);
 export const gameRouter = Router();
 
 gameRouter.get('/', new FindGamesController().handle);
+
+gameRouter.get('/id/:id', new FindGameByIdController().handle);
+gameRouter.get('/slug/:slug', new FindGameBySlugController().handle);
 
 gameRouter.post(
   '/',
@@ -25,14 +31,20 @@ gameRouter.post(
   new CreateGameController().handle,
 );
 
+gameRouter.get(
+  '/favorite',
+  ensureAuthenticated,
+  new FindFavoritesGamesController().handle,
+);
+
 gameRouter.post(
   '/favorite',
   ensureAuthenticated,
   new CreateFavoriteGameController().handle,
 );
 
-gameRouter.delete(
-  '/favorite-delete/:id',
+gameRouter.put(
+  '/favorite',
   ensureAuthenticated,
   new DeleteFavoriteGameController().handle,
 );
