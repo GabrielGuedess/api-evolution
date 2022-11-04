df3c4b7a7cd7a1e1b7b7eb4ce1b239358437b249
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-11-04 20:02:41 -0300

    feat: add new route /exist in client

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -4,6 +4,7 @@ import multer from 'multer';
 
 import { CreateClientController } from 'modules/clients/useCases/createClient/CreateClientController';
 import { FindClientController } from 'modules/clients/useCases/findClient/FindClientController';
+import { FindClientForCreateController } from 'modules/clients/useCases/findClientForCreate/FindClientForCreateController';
 import { UploadAvatarClientController } from 'modules/clients/useCases/uploadAvatarClient/UploadAvatarClientController';
 
 import uploadConfig from 'config/upload';
@@ -13,6 +14,8 @@ export const clientRouter = Router();
 const upload = multer(uploadConfig);
 
 clientRouter.get('/me', ensureAuthenticated, new FindClientController().handle);
+
+clientRouter.post('/exist', new FindClientForCreateController().handle);
 
 clientRouter.post(
   '/',
