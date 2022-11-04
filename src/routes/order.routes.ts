4d7c6433ed127942aacadf28405f16ca8ee9f643
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-11-04 20:05:10 -0300

    feat: add new routes / and /payment-intent in order

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -3,8 +3,16 @@ import { ensureAuthenticated } from 'middlewares/ensureAuthenticated';
 
 import { CreateOrderController } from 'modules/orders/useCases/createOrder/CreateOrderController';
 import { CreatePaymentIntentController } from 'modules/orders/useCases/createPaymentIntent/CreatePaymentIntentController';
+import { FindOrdersMeController } from 'modules/orders/useCases/findOrdersMe/FindOrdersMeController';
 
 export const orderRouter = Router();
 
+orderRouter.get('/', ensureAuthenticated, new FindOrdersMeController().handle);
+
 orderRouter.post('/', ensureAuthenticated, new CreateOrderController().handle);
-orderRouter.post('/payment-intent', new CreatePaymentIntentController().handle);
+
+orderRouter.post(
+  '/payment-intent',
+  ensureAuthenticated,
+  new CreatePaymentIntentController().handle,
+);
