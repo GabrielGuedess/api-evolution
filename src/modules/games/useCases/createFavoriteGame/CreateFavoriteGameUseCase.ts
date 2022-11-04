e28ccc48c3801c1a23659667280e3244f74247f1
Gabriel Guedes <gabrielrguedess@gmail.com>
2022-11-04 19:53:39 -0300

    fix: change create favorite games for many to many

--- src/modules/games/useCases/createFavoriteGame/CreateFavoriteGameUseCase.ts
+++ src/modules/games/useCases/createFavoriteGame/CreateFavoriteGameUseCase.ts
@@ -1,28 +1,42 @@
 import { prisma } from 'database/prismaClient';
 
+import { redis } from 'cache';
+
 import { ICreateFavoriteGameDTO } from 'modules/games/dtos/ICreateFavoriteGameDTO';
-
-import { AppError } from 'shared/errors/AppError';
-
 export class CreateFavoriteGameUseCase {
   async execute({ client_id, game_id }: ICreateFavoriteGameDTO) {
-    const gameExists = await prisma.game_Favorite.findFirst({
+    await redis.flushdb();
+
+    const gameCreated = await prisma.game_Favorite.upsert({
       where: {
-        game_id: game_id,
-      },
-    });
-
-    if (gameExists) {
-      throw new AppError('Game already on wishlist!');
-    }
-
-    const game = await prisma.game_Favorite.create({
-      data: {
         client_id,
-        game_id,
+      },
+      create: {
+        client_id,
+        games: {
+          connect: {
+            id: game_id,
+          },
+        },
+      },
+      update: {
+        games: {
+          connect: {
+            id: game_id,
+          },
+        },
+      },
+      select: {
+        games: {
+          include: {
+            genres: true,
+            developers: true,
+            platforms: true,
+          },
+        },
       },
     });
 
-    return game;
+    return gameCreated.games;
   }
 }
